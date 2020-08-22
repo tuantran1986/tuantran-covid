ab2a39bdc3d32d0322c217fa3d8bbb5033964203
tuantran1986 <61728265+tuantran1986@users.noreply.github.com>
2020-08-22 18:45:03 +0700

    up lan 1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// antd
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>,
+  </>,
   document.getElementById('root')
 );
 
